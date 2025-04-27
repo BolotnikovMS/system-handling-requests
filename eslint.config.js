48c9ff4194cf8451dcbb6470dbad7d938ad753b3
Mikhail <bolotnikovms@yandex.ru>
2025-04-27 19:47:43 +0500

    upd

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,10 @@ import stylisticTs from '@stylistic/eslint-plugin-ts'
 import parserTs from '@typescript-eslint/parser'
 
 export default [
- stylisticTs.configs.customize({
-    indent: 2,
-    quotes: 'single',
-    semi: true,
-    jsx: true,
- })
+	stylisticTs.configs.customize({
+		indent: 2,
+		quotes: 'single',
+		semi: true,
+		jsx: true,
+	})
 ]
